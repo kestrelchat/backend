27c8262ba1bf39909c28ca205f2dcf39dcbedc24
stribes <stribes@tutanota.de>
2026-05-29 18:18:50 +0200

    feat(meta): update meta
    Signed-off-by: stribes <stribes@tutanota.de>

--- crates/services/api/src/routes/meta.rs
+++ crates/services/api/src/routes/meta.rs
@@ -11,16 +11,32 @@ use crate::utils::errors::AppError;
 #[derive(Serialize, JsonSchema)]
 pub struct Meta {
   pub kestrel: String,
+  pub instance: InstanceMeta,
   pub features: FeaturesMeta,
 }
 
 #[derive(Serialize, JsonSchema)]
+pub struct InstanceMeta {
+    pub name: String,
+    pub domain: String,
+    pub description: Option<String>,
+}
+
+#[derive(Serialize, JsonSchema)]
 pub struct FeaturesMeta {
+  pub hcaptcha: HCaptchaMeta,
   pub registration: RegistrationMeta,
 }
 
 #[derive(Serialize, JsonSchema)]
+pub struct HCaptchaMeta {
+  pub enabled: bool,
+  pub sitekey: Option<String>,
+}
+
+#[derive(Serialize, JsonSchema)]
 pub struct RegistrationMeta {
+  pub enabled: bool,
   pub minimum_age: u32,
 }
 
@@ -29,8 +45,18 @@ pub struct RegistrationMeta {
 pub fn meta(config: &rocket::State<Config>) -> Json<Meta> {
   Json(Meta {
     kestrel: env!("CARGO_PKG_VERSION").into(),
+    instance: InstanceMeta {
+        name: config.instance.name.clone(),
+        domain: config.instance.domain.clone(),
+        description: config.instance.description.clone(),
+    },
     features: FeaturesMeta {
+      hcaptcha: HCaptchaMeta {
+        enabled: config.features.hcaptcha.enabled,
+        sitekey: config.features.hcaptcha.sitekey.clone(),
+      },
       registration: RegistrationMeta {
+        enabled: config.features.registration.enabled,
         minimum_age: config.features.registration.minimum_age,
       },
     },
